bbcd447d4ad9146c1b08975e38598b204e16507a
chirs <lxc940lxc@163.com>
2021-06-28 15:41:50 +0800

    overs

--- src/li.study.com/day23/main.go
+++ src/li.study.com/day23/main.go
@@ -1,7 +1,7 @@
 /*
  * @Author: chirs
  * @Date: 2021-06-28 15:24:18
- * @LastEditTime: 2021-06-28 15:24:48
+ * @LastEditTime: 2021-06-28 15:41:14
  * @LastEditors: chirs
  * @Description:
  */
@@ -11,4 +11,5 @@ import "fmt"
 
 func main() {
 	fmt.Printf("我又回来了")
+	fmt.Printf("我又回来了2222")
 }
