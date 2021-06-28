05a37cf8f15e79946b2781e5650bbeee8dbaca50
chirs <lxc940lxc@163.com>
2021-06-28 15:25:25 +0800

    test

---
+++ src/li.study.com/day23/main.go
@@ -0,0 +1,14 @@
+/*
+ * @Author: chirs
+ * @Date: 2021-06-28 15:24:18
+ * @LastEditTime: 2021-06-28 15:24:48
+ * @LastEditors: chirs
+ * @Description:
+ */
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("我又回来了")
+}
